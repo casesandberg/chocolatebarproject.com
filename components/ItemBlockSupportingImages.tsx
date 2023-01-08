4fe591250e0140ff54752d92f69ddd7c5f2bfd97
case <case@casesandberg.com>
2023-01-07 21:55:27 -0800

    Image Zoom

--- components/ItemBlockSupportingImages.tsx
+++ components/ItemBlockSupportingImages.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import { Image as ImageType } from '#/lib/data/bars'
 import Image from 'next/image'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 type ItemBlockSupportingImagesProps = {
   scheme?: 'split'
@@ -13,18 +17,22 @@ export function ItemBlockSupportingImages({
   const [firstImage, secondImage] = images
   return scheme === 'split' ? (
     <div className="grid grid-cols-2 gap-4">
-      <Image
-        src={firstImage.src}
-        alt={firstImage.alt}
-        width={200}
-        height={300}
-      />
-      <Image
-        src={secondImage.src}
-        alt={secondImage.alt}
-        width={200}
-        height={300}
-      />
+      <Zoom zoomImg={{ src: firstImage.src }} classDialog="zoom-image">
+        <Image
+          src={firstImage.src}
+          alt={firstImage.alt}
+          width={200}
+          height={300}
+        />
+      </Zoom>
+      <Zoom zoomImg={{ src: secondImage.src }} classDialog="zoom-image">
+        <Image
+          src={secondImage.src}
+          alt={secondImage.alt}
+          width={200}
+          height={300}
+        />
+      </Zoom>
     </div>
   ) : null
 }
