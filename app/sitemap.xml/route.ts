13858a4fab4acf19da1051f293bb4427360149a1
case <case@casesandberg.com>
2023-03-04 11:10:20 -0800

    Add Freq and LastMod to remaining pages

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,26 +1,32 @@
-import { getBars } from '#/lib/getBars'
+import { fetchMostRecentBar, getBars } from '#/lib/getBars'
 
 export async function GET() {
   const bars = getBars()
+  const mostRecentBar = await fetchMostRecentBar()
 
   return new Response(`<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
           xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
     <url>
       <loc>https://chocolatebarproject.com</loc>
+      <lastmod>${mostRecentBar?.releaseDate}</lastmod>
+      <changefreq>daily</changefreq>
     </url>
     <url>
       <loc>https://chocolatebarproject.com/bars</loc>
+      <lastmod>${mostRecentBar?.releaseDate}</lastmod>
+      <changefreq>daily</changefreq>
     </url>
     <url>
       <loc>https://chocolatebarproject.com/support</loc>
+      <lastmod>2023-01-01</lastmod>
     </url>
     ${bars
       .map((bar) => {
         return `
       <url>
-        <lastmod>${bar.releaseDate}</lastmod>
         <loc>https://chocolatebarproject.com/bars/${bar.slug}</loc>
+        <lastmod>${bar.releaseDate}</lastmod>
         <image:image>
           <image:loc>https://chocolatebarproject.com${bar.images.HERO.src}</image:loc>
         </image:image>
